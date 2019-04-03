0479e655bb69e69eae9e6d0e8b7e93dd648b0722
Pippin Barr <pippin.barr@gmail.com>
2019-04-03 14:05:28 -0400

    Commenting and refactoring
    ? Because I'm a real programmer who will do this for a little while and then give up

--- js/script.js
+++ js/script.js
@@ -7,93 +7,106 @@ Pippin Barr
 
 ******************/
 
+// The representation of our chess game
 let game;
+// The chessboard
 let board;
 
+// Tracking interactions to know the squares of movement
 let from = null;
 let to = null;
-let turn = 'w';
 
 $(document).ready(function () {
+  // Create our chessboard
   let config = {
     draggable: false,
     position: 'start',
   };
   board = ChessBoard('board', config);
 
+  // Create our game representation
   game = new Chess();
 
+  // Listen for clicks on the squares of the chessboard
   $('.square-55d63').on('click',squareClicked);
 });
 
+// Called when a square is clicked
 function squareClicked() {
+  // Find out the notation of the square and also the element representing the piece
   let square = $(this).attr('data-square');
-
-  console.log(square)
-
   let piece = $(this).find('.piece-417db');
+  let validPiece = piece.length !== 0 && piece.attr('data-piece').indexOf(game.turn()) !== -1;
 
-  if (from === null && piece.length !== 0 && piece.attr('data-piece').indexOf(turn) !== -1) {
-    from = square;
-
-    var moves = game.moves({
-      square: from,
-      verbose: true
-    });
-
-    // exit if there are no moves available for this square
-    if (moves.length === 0) return;
-
-    // highlight the possible squares for this piece
-    for (var i = 0; i < moves.length; i++) {
-      highlight(moves[i].to);
-    }
+  if (from === null && validPiece) {
+    // We haven't selected a move yet + a piece of the correct colour was selected
+    highlightMoves(square);
   }
   else if (from !== null) {
-    if (piece.length !== 0 && piece.attr('data-piece').indexOf(turn) !== -1) {
-      // Selecting a new piece to move
-      clearHighlights();
-      from = square;
-
-      var moves = game.moves({
-        square: from,
-        verbose: true
-      });
-
-      // exit if there are no moves available for this square
-      if (moves.length === 0) return;
-
-      // highlight the possible squares for this piece
-      for (var i = 0; i < moves.length; i++) {
-        highlight(moves[i].to);
-      }
+    // We have already selected a square to move from (and thus a piece)
+    if (validPiece) {
+      // But now we're selecting another valid piece to move, so we should rehilight
+      highlightMoves(square);
     }
     else if ($(this).hasClass('highlight1-32417')) {
-
       let to = $(this).attr('data-square');
-
-      let move = game.move({
-        from: from,
-        to: to,
-        promotion: 'q' // NOTE: always promote to a queen for example simplicity
-      });
-
-      clearHighlights();
-
-      board.position(game.fen(),false);
-
-      from = null;
-      to = null;
-
-      turn = (turn === 'w') ? 'b' : 'w';
+      makeMove(to);
     };
   }
 }
 
+// Highlights the moves available to the piece on the given square
+// and sets it up as the current 'from'
+function highlightMoves(square) {
+  clearHighlights();
+
+  from = square;
+
+  var moves = game.moves({
+    square: from,
+    verbose: true
+  });
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return;
+
+  moves.forEach(function (move) {
+    highlight(move.to);
+  });
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    highlight(moves[i].to);
+  }
+}
+
+function makeMove(to) {
+  // Otherwise we're only interested if we select a highlighted square (a valid move)
+
+  // Make the move in the game representation
+  let move = game.move({
+    from: from,
+    to: to,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
+
+  // Clear all highlights from the board (a new turn is about to begin)
+  clearHighlights();
+
+  // Update the board based on the new position
+  board.position(game.fen(),false);
+
+  // Reset the move tracking
+  from = null;
+  to = null;
+}
+
+// Remove highlights from every square on the board
 function clearHighlights() {
   $('.square-55d63').removeClass(`highlight1-32417`);
 }
 
+// Highlight the specified square
 function highlight(square) {
   $('.square-'+square).addClass(`highlight1-32417`);
 }
