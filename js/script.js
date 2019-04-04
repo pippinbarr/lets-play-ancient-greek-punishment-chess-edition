87591dd4498485bfe6056a9c7c6b960e9902df49
Pippin Barr <pippin.barr@gmail.com>
2019-04-04 13:28:39 -0400

    Created the world's most terrible menu
    ? So  you can at least select versions of the game, meaning builds will be more accessible in the commit history

--- js/script.js
+++ js/script.js
@@ -11,11 +11,43 @@ Pippin Barr
 let board;
 
 $(document).ready(function () {
+
+  $('.menu_button').on('click',menuButtonClicked);
   // Create our default game
   let chess = new Tantalus();
+});
+
+
+function menuButtonClicked() {
+  let level = $(this).attr('id');
+  let chess;
+  switch(level) {
+    case 'sisyphus':
+    chess = new Sisyphus();
+    break;
+
+    case 'prometheus':
+    chess = new Prometheus();
+    break;
+
+    case 'tantalus':
+    chess = new Tantalus();
+    break;
+
+    default:
+    chess = false;
+    break;
+  }
+
+  if (!chess) {
+    console.log("Error! Menu click broke!");
+  }
+
+  $('#menu').hide();
+  $('#board').show();
 
   // Listen for clicks on the squares of the chessboard
   $('.square-55d63').on('click', function (event) {
     chess.squareClicked(event);
   });
-});
+}
